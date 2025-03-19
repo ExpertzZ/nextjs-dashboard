7860c4a5c0cd2ae95c60d8cf0f22e7f88499cfad
ExpertzZ <74832302+ExpertzZ@users.noreply.github.com>
2025-03-19 15:09:36 +0100

    stop on hover ProcessStep Component

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -37,7 +37,7 @@ export type LatestInvoice = {
   image_url: string;
   email: string;
   amount: string;
-};
+};  
 
 // The database returns a number for amount, but we later format it to a string with the formatCurrency function
 export type LatestInvoiceRaw = Omit<LatestInvoice, 'amount'> & {
